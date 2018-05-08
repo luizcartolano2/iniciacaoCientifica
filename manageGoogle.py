9b9a67a68f038ece8cd335e8207a50662700e64b
Luiz Cartolano <duducartolano@gmail.com>
2018-05-08 15:40:06 -0300

    Functions to read and slice working well

--- manageGoogle.py
+++ manageGoogle.py
@@ -34,3 +34,40 @@ class ManageGoogle(object):
 
         # returns a list of dictionaries
         return routes
+
+    '''
+        Find the date of a tweet based on his timestamp
+    '''
+    def getDate(self, chartime, tz):
+        timestamp = float(chartime)
+        try:
+            # if the timestamp is in seconds, the timestamp conversion to p / date occurs without errors
+            date = datetime.fromtimestamp(timestamp)
+        except:
+            # otherwise, I need to convert from microseconds to sec first
+            date = self.convertTimestamp(timestamp)
+        date_aware = timezone(tz).localize(date)
+        # converts according to tweet timezone
+        new_date = date_aware.astimezone(timezone(tz))
+        return new_date
+
+    '''
+        Convert the timestamp of a tweet from microseconds to seconds
+    '''
+    def convertTimestamp(self, timestamp):
+        if timestamp > 1519231541:  # Date of today - UTC: Wednesday 21st February 2018 04:45:41 PM
+            timestamp /= 1000       # convert from microseconds to seconds
+        return datetime.fromtimestamp(timestamp)
+
+    def slicingDocs(self, dates, docs):
+        list_routes = {}
+        for doc in docs:
+            date = str(self.getDate(doc["time"],doc["timezone"]))
+            date = date.split(' ',1)
+            if date[0] in dates:
+                try:
+                    list_routes[date[0]] += [doc]
+                except:
+                    list_routes[date[0]] = [doc]
+        
+        return list_routes
